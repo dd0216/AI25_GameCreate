7222ee50c0219767ca6d0bf5e03e2405883037a8
seokchaehee <seokchaehee@gmail.com>
2025-09-15 02:57:52 +0900

    2weeks 떨어지는 네모 로고

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class PlayerCont : MonoBehaviour
+{
+    float start_point = 0.0f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        transform.position = new Vector3(start_point, 7.0f, 0.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        start_point = start_point + 0.01f;
+        transform.position = new Vector3(start_point, transform.position.y, transform.position.z);
+    }
+}
